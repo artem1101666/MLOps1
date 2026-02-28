132a66cdac3aa5a7c797ee954d8b74f66f0c3708
artem1101666 <artemy1408@gmail.com>
2026-02-28 15:13:21 +0000

    update1

--- MLOPS/lab airflow/airflow_pipe.py
+++ MLOPS/lab airflow/airflow_pipe.py
@@ -17,63 +17,53 @@ from datetime import timedelta
 from train_model import train
 
 def download_data():
-    df = pd.read_csv('https://raw.githubusercontent.com/dayekb/Basic_ML_Alg/main/cars_moldova_no_dup.csv', delimiter = ',')
-    df.to_csv("cars.csv", index = False)
+    df = pd.read_csv('/home/artem/Downloads/archive/US_Top_50_Universities_2026.csv', delimiter = ',')
+    df.to_csv("universities.csv", index = False)
     print("df: ", df.shape)
     return df
 
 def clear_data():
-    df = pd.read_csv("cars.csv")
-    
-    cat_columns = ['Make', 'Model', 'Style', 'Fuel_type', 'Transmission']
-    num_columns = ['Year', 'Distance', 'Engine_capacity(cm3)', 'Price(euro)']
-    
-    question_dist = df[(df.Year <2021) & (df.Distance < 1100)]
-    df = df.drop(question_dist.index)
-    # Анализ и очистка данных
-    # анализ гистограмм
-    question_dist = df[(df.Distance > 1e6)]
-    df = df.drop(question_dist.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] < 200]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_engine = df[df["Engine_capacity(cm3)"] > 5000]
-    df = df.drop(question_engine.index)
-    
-    # здравый смысл
-    question_price = df[(df["Price(euro)"] < 101)]
-    df = df.drop(question_price.index)
-    
-    # анализ гистограмм
-    question_price = df[df["Price(euro)"] > 1e5]
-    df = df.drop(question_price.index)
-    
-    #анализ гистограмм
-    question_year = df[df.Year < 1971]
-    df = df.drop(question_year.index)
-    
-    df = df.reset_index(drop=True)  
+    df = pd.read_csv("universities.csv")
+
+    # Категориальные и числовые признаки
+    cat_columns = ['University_Name', 'Institution_Type', 'State']
+    num_columns = ['Founded_Year',
+                   'Research_Impact_Score',
+                   'Intl_Student_Ratio',
+                   'Employment_Rate']
+
+    # Удаляем пропуски
+    df = df.dropna()
+
+    # Проверка года основания (разумные границы)
+    df = df[(df['Founded_Year'] > 1600) & (df['Founded_Year'] <= 2026)]
+
+    # Ограничим значения метрик от 0 до 100 (если это проценты / индексы)
+    df = df[(df['Research_Impact_Score'] >= 0) & (df['Research_Impact_Score'] <= 100)]
+    df = df[(df['Intl_Student_Ratio'] >= 0) & (df['Intl_Student_Ratio'] <= 100)]
+    df = df[(df['Employment_Rate'] >= 0) & (df['Employment_Rate'] <= 100)]
+
+    df = df.reset_index(drop=True)
+
+    # Кодирование категориальных признаков
     ordinal = OrdinalEncoder()
-    ordinal.fit(df[cat_columns]);
-    Ordinal_encoded = ordinal.transform(df[cat_columns])
-    df_ordinal = pd.DataFrame(Ordinal_encoded, columns=cat_columns)
-    df[cat_columns] = df_ordinal[cat_columns]
-    df.to_csv('df_clear.csv')
+    df[cat_columns] = ordinal.fit_transform(df[cat_columns])
+
+    df.to_csv("df_clear.csv", index=False)
+
+    print("Cleaned shape:", df.shape)
     return True
 
-dag_cars = DAG(
+dag_universities = DAG(
     dag_id="train_pipe",
     start_date=datetime(2025, 2, 3),
-    concurrency=4,
-    schedule_interval=timedelta(minutes=5),
-#    schedule="@hourly",
+    max_active_tasks=4,
+    schedule=timedelta(minutes=5),
+#   schedule="@hourly",
     max_active_runs=1,
     catchup=False,
 )
-download_task = PythonOperator(python_callable=download_data, task_id = "download_cars", dag = dag_cars)
-clear_task = PythonOperator(python_callable=clear_data, task_id = "clear_cars", dag = dag_cars)
-train_task = PythonOperator(python_callable=train, task_id = "train_cars", dag = dag_cars)
+download_task = PythonOperator(python_callable=download_data, task_id = "download_universities", dag = dag_universities)
+clear_task = PythonOperator(python_callable=clear_data, task_id = "clear_universities", dag = dag_universities)
+train_task = PythonOperator(python_callable=train, task_id = "train_universities", dag = dag_universities)
 download_task >> clear_task >> train_task
